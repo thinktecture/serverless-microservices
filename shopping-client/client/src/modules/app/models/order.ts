ac1803a70c72985ff0b0af14b1b149ab1d9526eb
Yannick Baron <yannick.baron@thinktecture.com>
2019-04-25 19:34:19 +0200

    fix order model (missing created property)

--- shopping-client/client/src/modules/app/models/order.ts
+++ shopping-client/client/src/modules/app/models/order.ts
@@ -1,8 +1,6 @@
-import { Observable } from "rxjs";
-
 export interface Order {
     id: string;
     description: string;
     items: { id: string, quantity: number }[];
-    shippingCreated$: Observable<boolean>;
+    created: any; // FIXME probably string I guess?
 }
